754b010e4bc69607d76402a88c613168a15e9eb7
y.korol <y.korol@gmail.com>
2024-02-21 22:59:10 +0200

    added new abstract class Interaction  for Like and Dislike, changed methods get_or_create to create and changed class Q

--- Django_project/myapp/models.py
+++ Django_project/myapp/models.py
@@ -1,10 +1,9 @@
 from datetime import timedelta
-
+from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.fields import GenericRelation
 
 
 class MyApp(models.Model):
@@ -70,28 +69,27 @@ class Article(models.Model):
         return str(self.title)
 
 
-class Like(models.Model):
+class Interaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        # return f"{self.user.username} likes {self.content_object.title}"
-        return self.user
+    class Meta:
+        abstract = True
 
 
-class Dislike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-    created_at = models.DateTimeField(auto_now_add=True)
+class Like(Interaction):
 
     def __str__(self):
-        # return f"{self.user.username} dislikes {self.content_object.title}"
-        return self.user
+        return f"Like by {self.user}"
+
+
+class Dislike(Interaction):
+
+    def __str__(self):
+        return f"Dislike by {self.user}"
 
 
 class Comment(models.Model):
@@ -100,17 +98,15 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
-    like = GenericRelation(Like)
 
     def __str__(self):
         return self.article.title
 
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.created_at -= timedelta(days=365)
+        if not self.pk:
+            self.created_at = timezone.now() - timedelta(days=365)
         super().save(*args, **kwargs)
+
 
 
 
